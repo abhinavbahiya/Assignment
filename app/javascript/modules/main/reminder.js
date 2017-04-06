7b966736aa161f0d400aa259427a5a605a44d846
Abhinav <abhinav.bahiya@weboniselab.com>
2017-04-06 18:50:39 +0530

    URL Fixing

--- app/javascript/modules/main/reminder.js
+++ app/javascript/modules/main/reminder.js
@@ -1,140 +1,140 @@
-import React from 'react';
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
-//import Base64 from 'base-64';
+  import React from 'react';
+  import DatePicker from 'react-datepicker';
+  import moment from 'moment';
+  //import Base64 from 'base-64';
 
-//import { setDataInCookies } from '.';
+  //import { setDataInCookies } from '.';
 
-class Reminder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      note: '',
-      lengthRemaining: 100,
-      startDate: moment(),
-      Occurences: ['EveryDay', 'EveryMonth', 'EveryYear', 'None'],
-      selectedOccurence: '',
-      isNone: false,
-      isActive: true
+  class Reminder extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        note: '',
+        lengthRemaining: 100,
+        startDate: moment(),
+        Occurences: ['EveryDay', 'EveryMonth', 'EveryYear', 'None'],
+        selectedOccurence: '',
+        isNone: false,
+        isActive: true
+      }
+
+      this.handleNoteChange = this.handleNoteChange.bind(this);
+      this.handleDateChange = this.handleDateChange.bind(this);
+      this.handleOccurence = this.handleOccurence.bind(this);
+      this.getOccurences = this.getOccurences.bind(this);
+      this.setDataInCookies = this.setDataInCookies.bind(this);
+      this.validateFields = this.validateFields.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    componentWillMount() {
+      sessionStorage.removeItem('note');
+      sessionStorage.removeItem('startDate');
+      sessionStorage.removeItem('selectedOccurence');
+      // Do some coding here
     }
 
-    this.handleNoteChange = this.handleNoteChange.bind(this);
-    this.handleDateChange = this.handleDateChange.bind(this);
-    this.handleOccurence = this.handleOccurence.bind(this);
-    this.getOccurences = this.getOccurences.bind(this);
-    this.setDataInCookies = this.setDataInCookies.bind(this);
-    this.validateFields = this.validateFields.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  };
+    // contextTypes: {
+    //       router: React.PropTypes.object
+    //   },
 
-  ComponentWillMount() {
-    // sessionStorage.removeItem('note');
-    // sessionStorage.removeItem('startDate');
-    // sessionStorage.removeItem('selectedOccurence');
-    // Do some coding here
-  }
+  //   export default function
+    setDataInCookies() {
+      // let decoded = Base64.encode(JSON.stringify(sessionData));
+      sessionStorage.setItem('note', JSON.stringify(this.state.note));
+      sessionStorage.setItem('startDate', JSON.stringify(this.state.startDate));
+      sessionStorage.setItem('selectedOccurence', JSON.stringify(this.state.selectedOccurence));
+    }
 
-  // contextTypes: {
-  //       router: React.PropTypes.object
-  //   },
-
-//   export default function
-  setDataInCookies() {
-    // let decoded = Base64.encode(JSON.stringify(sessionData));
-    sessionStorage.setItem('note', JSON.stringify(this.state.note));
-    sessionStorage.setItem('startDate', JSON.stringify(this.state.startDate));
-    sessionStorage.setItem('selectedOccurence', JSON.stringify(this.state.selectedOccurence));
-  }
-
-  handleNoteChange(event) {
-    let text = event.target.value;
-    let lengthOfNote = text.length;
-    this.setState({
-      note: event.target.value,
-      lengthRemaining: 100-lengthOfNote
-    });
-    this.validateFields();
-  }
-
-  handleDateChange(date) {
-    console.log(date.toDate());
-    this.setState({
-      startDate: date
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.setDataInCookies();
-    // make AJAX call here
-    //this.context.router.push('url/here');
-    location.hash = 'showReminder';
-  }
-
-  getOccurences() {
-    return this.state.Occurences.map((item, index) => {
-      return <option key = {index} value = {item}> {item} </option>
-    });
-  }
-
-  handleOccurence(event) {
-      let selectedValue = this.refs.selectInput.value;
-      this.state.selectedOccurence = selectedValue;
-      if (selectedValue === 'None') {
-          this.state.startDate = moment();
-          this.state.isNone = true;
-      }
-      else {
-          this.state.isNone = false;
-      }
-      this.setState(this.state);
+    handleNoteChange(event) {
+      let text = event.target.value;
+      let lengthOfNote = text.length;
+      this.setState({
+        note: event.target.value,
+        lengthRemaining: 100-lengthOfNote
+      });
       this.validateFields();
-  }
-
-  validateFields() {
-    if (this.state.note.length && this.state.selectedOccurence.length) {
-        this.setState({
-            isActive: false
-        });
     }
-  }
 
-   render() {
-      return (
-        <div>
-           <form onSubmit={this.handleSubmit}>
-            <div>
-              <label>
-                Note: &nbsp; &nbsp;
-              <input type="text" maxLength="100" value = {this.state.value} onChange = {this.handleNoteChange} />
-              </label>
-             </div>
-             <div>
-               <label>
-                 Characters Remaining: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; {this.state.lengthRemaining}
+    handleDateChange(date) {
+      console.log(date.toDate());
+      this.setState({
+        startDate: date
+      });
+    }
+
+    handleSubmit(event) {
+      event.preventDefault();
+      this.setDataInCookies();
+      // make AJAX call here
+      //this.context.router.push('url/here');
+      location.hash = 'showReminder';
+    }
+
+    getOccurences() {
+      return this.state.Occurences.map((item, index) => {
+        return <option key = {index} value = {item}> {item} </option>
+      });
+    }
+
+    handleOccurence(event) {
+        let selectedValue = this.refs.selectInput.value;
+        this.state.selectedOccurence = selectedValue;
+        if (selectedValue === 'None') {
+            this.state.startDate = moment();
+            this.state.isNone = true;
+        }
+        else {
+            this.state.isNone = false;
+        }
+        this.setState(this.state);
+        this.validateFields();
+    }
+
+    validateFields() {
+      if (this.state.note.length && this.state.selectedOccurence.length) {
+          this.setState({
+              isActive: false
+          });
+      }
+    }
+
+     render() {
+        return (
+          <div>
+             <form onSubmit={this.handleSubmit}>
+              <div>
+                <label>
+                  Note: &nbsp; &nbsp;
+                <input type="text" maxLength="100" value = {this.state.value} onChange = {this.handleNoteChange} />
+                </label>
+               </div>
+               <div>
+                 <label>
+                   Characters Remaining: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; {this.state.lengthRemaining}
+                 </label>
+               </div>
+               <div>
+               <label> Date: &nbsp; &nbsp;
                </label>
-             </div>
-             <div>
-             <label> Date: &nbsp; &nbsp;
-             </label>
-             <DatePicker ref = 'datePicker' dateFormat="YYYY/MM/DD" selected = {this.state.startDate} onChange = {this.handleDateChange} disabled = {this.state.isNone} />
-             </div>
-             <div>
-              <label>
-                Occurence:&nbsp; &nbsp; &nbsp;
-                <select ref="selectInput" onChange={this.handleOccurence}> <option value="">Select Occurence</option> {this.getOccurences()} </select>
-              </label>
-             </div>
-             <div>
-               <input type="submit" value="Submit" disabled = {this.state.isActive} />
-             </div>
-           </form>
-        </div>
-      );
+               <DatePicker ref = 'datePicker' dateFormat="YYYY/MM/DD" selected = {this.state.startDate} onChange = {this.handleDateChange} disabled = {this.state.isNone} />
+               </div>
+               <div>
+               <label>
+                  Occurence:&nbsp; &nbsp; &nbsp;
+                  <select ref="selectInput" onChange={this.handleOccurence}> <option value="">Select Occurence</option> {this.getOccurences()} </select>
+                </label>
+               </div>
+               <div>
+                 <input type="submit" value="Submit" disabled = {this.state.isActive} />
+               </div>
+             </form>
+          </div>
+        );
+      }
     }
-  }
 
-export default Reminder;
+  export default Reminder;
 
 
-// className = {this.state.isActive ? 'buttonEnabled' : 'buttonDisabled'}
+  // className = {this.state.isActive ? 'buttonEnabled' : 'buttonDisabled'}
--- app/javascript/modules/main/showReminder.js
+++ app/javascript/modules/main/showReminder.js
@@ -1,30 +1,45 @@
-import React from 'react';
-//import Base64 from 'base-64';
+  import React from 'react';
+  //import Base64 from 'base-64';
 
-class showReminder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.showData = this.showData.bind(this);
-  };
+  class showReminder extends React.Component {
+    constructor(props) {
+      super(props);
 
-  showData() {
-    let note = sessionStorage.getItem('note');
-    let startDate = sessionStorage.getItem('startDate');
-    let selectedOccurence = sessionStorage.getItem('selectedOccurence');
-    return " This is your Reminder NOTE: " + note + " Date for Reminder is " + startDate.slice(0, 11) + "\" and it will occur " + selectedOccurence;
+      this.state = {
+        note: '',
+        startDate: '',
+        selectedOccurence: ''
+      }
+
+      this.showData = this.showData.bind(this);
+    };
+
+    componentWillMount() {
+      if (!sessionStorage.note && !sessionStorage.startDate && !sessionStorage.selectedOccurence) {
+        // location.hash = '/';
+        window.location = '/';
+      }
+    }
+
+
+    showData() {
+      this.state.note = sessionStorage.getItem('note');
+      this.state.startDate = sessionStorage.getItem('startDate');
+      this.state.selectedOccurence = sessionStorage.getItem('selectedOccurence');
+      return " This is your Reminder NOTE: " + this.state.note + " Date for Reminder is " + this.state.startDate.slice(0, 11) + "\" and it will occur " + this.state.selectedOccurence;
+    }
+
+     render() {
+        return (
+          <div>
+            Yes!
+            {this.showData()}
+          </div>
+        );
+     }
   }
 
-   render() {
-      return (
-        <div>
-          Yes!
-          {this.showData()}
-        </div>
-      );
-   }
-}
-
-export default showReminder;
+  export default showReminder;
 
 
-//{JSON.parse(Base64.decode(Cookie.get('note')))}
+  //{JSON.parse(Base64.decode(Cookie.get('note')))}
